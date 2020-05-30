dff68caa18940083cae815f920a2aeef57a5ab5a
anil kadam <anilkadam210@gmail.com>
2020-05-30 13:49:36 +0530

    Handling form submittal - onSubmit and onChange

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,8 @@ import SearchBar from "./SearchBar";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='ui container'>
         <SearchBar />
-        Online Video Player..
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className='ui segment'>
+        <form className='ui form' onSubmit={this.onFormSubmit}>
+          <div className='field'>
+            <label htmlFor='search-bar'>Video Search</label>
+            <input
+              id='search-bar'
+              type='text'
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
